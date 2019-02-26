dbcaaa52e87c71da099cac6bfd5e22927b7b03e6
huyanzhuo <huyanzhuo@ywwl.com>
2019-02-26 15:08:14 +0800

    #hello wx!

---
+++ src/main/java/com/example/testwx/wxcallback/WxCallBack.java
@@ -0,0 +1,17 @@
+package com.example.testwx.wxcallback;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author : huyanzhuo
+ * @date : 2019/2/26 3:05 PM
+ */
+@RestController
+public class WxCallBack {
+    @RequestMapping("/hello")
+    private String hello() {
+        return "hello wx!";
+    }
+
+}
