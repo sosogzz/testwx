e9bec470ba90f9930e3da8235e9453254973a44b
huyanzhuo <huyanzhuo@ywwl.com>
2019-02-26 15:03:35 +0800

    Initial commit

---
+++ src/main/java/com/example/testwx/TestwxApplication.java
@@ -0,0 +1,13 @@
+package com.example.testwx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestwxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestwxApplication.class, args);
+    }
+
+}
